a4746e75067a562479378443197d6bdea8b8423d
mukulembezewil <140501029+mukulembezewil@users.noreply.github.com>
2023-07-28 00:22:26 +0300

    add state and theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,39 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, createTheme } from '@mui/material';
+import { themeSettings } from './theme';
+import { ThemeProvider } from '@emotion/react';
 
 function App() {
-	return <div className="app"></div>;
+	const mode = useSelector((state) => state.mode);
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Routes>
+						<Route
+							path="/"
+							element={<LoginPage />}
+						/>
+						<Route
+							path="/home"
+							element={<HomePage />}
+						/>
+						<Route
+							path="/profile/:userId"
+							element={<ProfilePage />}
+						/>
+					</Routes>
+				</ThemeProvider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
